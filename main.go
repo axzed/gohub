26b3ba1f74e3678ce68d2fbe8cfa1bd517edaad5
axzed <waxwc1491224820@gmail.com>
2023-05-29 10:42:38 +0800

    :sparkles: feat: add viper to manage configs

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"flag"
 	"github.com/axzed/gohub/bootstrap"
+	"github.com/axzed/gohub/pkg/config"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
 
 	// 初始化 Gin 实例
 	router := gin.Default()
@@ -14,7 +22,7 @@ func main() {
 	bootstrap.SetupRoute(router)
 
 	// 运行服务
-	err := router.Run(":8000")
+	err := router.Run(":3000")
 	if err != nil {
 		panic(err)
 	}
